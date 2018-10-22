4bdd5efcc0fef3a2bc3b64a8c4717735eeae01dc
John Sangwon Suh <johnsuh94@gmail.com>
2018-10-22 14:17:35 -0400

    <feat> Add comments to store state

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,37 @@
+const axios = require("axios");
+const baseProtocol = process.env.Protocol || "http";
+const baseURL = process.env.BASE_URL || "localhost:8000";
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'labqueue_stats',
+    title: "labqueue_stats",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
+  env: {
+    baseURL: baseURL
+  },
+
+  modules: ["@nuxtjs/axios"],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    baseURL: `${baseProtocol}://${baseURL}/api/`
+  },
+  plugins: [],
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,16 +39,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
-
+};
--- store/index.js
+++ store/index.js
@@ -6,15 +6,55 @@ Vue.use(Vuex);
 const store = () =>
   new Vuex.Store({
     state: {
-      labTAs: {}
+      /* 
+      labTA:
+        full_name: SerializerMethodField
+        netid: Str
+        puid: Str
+        major: Str
+        grad_year: Int
+        type: ?
+        picture: Str
+        is_active: Bool
+      labTAs:
+          {LabTA.netId: {LabTA...}, } 
+      */
+      labTAs: {},
+      /* 
+      request:
+        pk: Int
+        author_full_name: Str
+        author_username: Str
+        location: Str
+        course: ?
+        description: Str
+        time_created: DateTime(? String?)
+        acceptor_netid: ?
+        time_accepted: DateTime(? String?)
+        closer_username: Str
+        time_closed: DateTime(? String?)
+      requests:
+        {request.pk : {request...}}
+      */
+      requests: {}
     },
     getters: {
+      /* Returns the appropriate LabTA based on the query */
       getLabTa: state => query => {}
     },
     mutations: {
+      /* Sets each of the labTAs to the store */
       setLabTAs(state, labTAs) {
-        state.labTAs = labTAs;
-      }
+        labTAs.forEach(labTA => {
+          context.commit("setLabTA", labTA);
+        });
+      },
+      /* Sets a LabTA to state. labTas with netid as key and the item itself as value */
+      setLabTA(state, labTA) {
+        state.labTAs[labTA.netid] = labTA;
+      },
+      /* Sets a Request to state. Request with pk as key and the item itself as value */
+      setRequests(state, requests) {}
     },
     actions: {
       ...apis
