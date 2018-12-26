48e9148f0ec99405de78f78d5f06780bf90370af
John Sangwon Suh <johnsuh94@gmail.com>
2018-12-26 01:02:44 -0500

    <feat> Add Top navbar

--- labqueue_stats/components/nav/DemoNavbarTop.vue
+++ labqueue_stats/components/nav/DemoNavbarTop.vue
@@ -1,10 +1,52 @@
 <template>
-  <div></div>
-</template>
-<script>
-export default {};
-</script>
-<style lang="scss" scoped>
-</style>
+  <nav id="demo-navbar-top-container">
+    <ul id="demo-navbar-top-list">
+      <nuxt-link tag="li" id="logo" to="/">
+        <img class="logo-img" src="~/assets/static/Logo.svg">
+      </nuxt-link>
 
+      <a :href="labqueueBaseURL">
+        <ButtonText text="Back to Queue" :minWidth="-1" :minHeight="5" fontColor="grey-dark"/>
+      </a>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import ButtonText from "@/components/UI_v2/ButtonText.vue";
+export default {
+  components: {
+    ButtonText
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss_v2/main.scss";
+
+.logo-img {
+  height: $navbar-top_height - 2rem;
+  vertical-align: middle;
+}
+
+#demo-navbar-top-container {
+  top: 0;
+  position: sticky;
+  z-index: 1;
+  height: $navbar-top_height;
+  line-height: $navbar-top_height;
+  border-bottom: 1px solid $color-grey-light;
+  padding: 0 2rem;
+}
+
+#demo-navbar-top-list {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
 
