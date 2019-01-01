a0ee6abb139485dc4ea5d87fbec3129e0e31c282
John Sangwon Suh <johnsuh94@gmail.com>
2019-01-01 11:35:32 -0500

    <feat> Initial version of requests real

--- labqueue_stats/pages/requests/index.vue
+++ labqueue_stats/pages/requests/index.vue
@@ -1,13 +1,70 @@
 <template>
-  <div></div>
+  <div id="page-requests">
+    <div id="page-requests-list">
+      <RequestList :requests="requests" :isDemo="false"/>
+    </div>
+  </div>
 </template>
+
 <script>
-import PageHeader from "@/components/pages/PageHeader.vue";
+import { mapGetters } from "vuex";
+import RequestList from "~/components/pages/requests/RequestsList";
+import RequestDetail from "~/components/pages/requests/RequestsDetail";
+import { demo } from "@/mixins/demo.js";
+import { dateToString, getShiftRequests, filter_shifts } from "@/utils.js";
+import {
+  DATE_FORMAT,
+  INITIAL_DATE_FROM,
+  INITIAL_DATE_TO
+} from "@/constants.js";
+
 export default {
-  components: {
-    PageHeader
+  layout: "demo_dashboard",
+  mixins: [demo],
+  data() {
+    return {};
   },
-  layout: "dashboard",
-  middleware: ["check-auth"]
+  components: {
+    RequestList,
+    RequestDetail
+  },
+  computed: {
+    ...mapGetters({
+      getRequests: "getRequests"
+    }),
+    requests() {
+      return this.getRequests("requests");
+    }
+  },
+  async fetch(context) {
+    if (context.store.getters.getRequests("requests")) {
+      return;
+    }
+    const query = {
+      accepted_before: dateToString(INITIAL_DATE_TO, DATE_FORMAT),
+      accepted_after: "2016-01-01"
+    };
+    const requests = await context.store.dispatch("queryRequests_demo", query);
+    context.store.commit("setRequests", {
+      page: "requests",
+      requests: requests
+    });
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss_v2/main.scss";
+#page-requests {
+  border-right: 1px solid $color-grey-light;
+  box-sizing: border-box;
+  height: 100vh;
+  display: flex;
+}
+#page-requests-list {
+  @include respond(laptop) {
+    width: 50rem;
+  }
+}
+</style>
+
